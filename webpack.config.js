f1ca69205dcc09651a357a7cf27a5f370545e7de
codexvisuals <melissailene@gmail.com>
2025-02-19 08:52:50 -0500

    Slim initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,7 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    'bouncy-watch': './src/demos/bouncy-watch/BouncyWatchEntry.js',
-    'shoe-picker': './src/demos/shoe-picker/ShoePickerEntry.js',
     'lusion-connectors': './src/demos/lusion-connectors/LusionConnectorsEntry.js',
-    'diamond-app': './src/demos/diamond-app/DiamondAppEntry.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
